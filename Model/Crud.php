7e2f563f5d65d5c67d02f722f042326ae047597e
Caio Alexandre <caioxandres2000@gmail.com>
2018-01-03 11:18:16 -0300

    Criando inserção de dados no CRUD de usuarios

--- Model/Crud.php
+++ Model/Crud.php
@@ -9,3 +9,23 @@ try {
     //Gera Erro
     echo 'Erro na conexão: ' . $ex->getMessage();
 }
+
+//Inserir dados | CREATE
+function create($pdo,$Nome,$Email,$DataNascimento,$Senha){
+    
+    try{
+        $sttm = $pdo->prepare("INSERT INTO `usuarios`(`ID`,`Nome`,`Email`,`DataNascimento`,`Senha`) VALUES(NULL,?,?,?,?);");
+
+        $sttm->bindParam(1, $Nome);
+        $sttm->bindParam(2, $Email);
+        $sttm->bindParam(3, $DataNascimento);
+        $sttm->bindParam(4, $Senha);
+        $sttm->execute();
+
+    } catch (Exception $ex) {
+        //Gera Erro
+        echo 'Erro ao Registrar: ' . $ex->getMessage();
+    }
+}
+//Exemplo de inserção;
+//create($pdo,"Caio Alexandre", "caioxandres2000@gmail.com", "2000-07-16", "OlaMundo");
