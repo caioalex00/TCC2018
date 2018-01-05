76897672dd11bde38c9037af0d00b218c1940d62
Caio Alexandre <caioxandres2000@gmail.com>
2018-01-05 10:09:12 -0300

    Descontinuando CRUD v1 e Começando novo pela classe Conexão

---
+++ Model/Conexao.php
@@ -0,0 +1,60 @@
+<?php
+class Conexao {
+    private $host;
+    private $dbname;
+    private $usuario;
+    private $senha;
+    private $tipo;
+    
+    public function __construct() {
+        $this->definirValores();
+    }
+    
+    private function definirValores(){
+        $this->host = "localhost";
+        $this->dbname = "tcc2018";
+        $this->usuario = "root";
+        $this->senha = "";
+        $this->tipo = "mysql";
+    }
+
+
+    public function conectar(){
+        try {
+            
+            $this->conexao = new PDO($this->getTipo() . ':host=' . $this->getHost() . ';dbname=' . $this->getDbname(), $this->getUsuario(), $this->getSenha());
+            
+        } catch (Exception $ex) {
+            echo 'Erro na conexão: ' . $ex->getMessage();
+        }
+        
+        return $this->conexao;
+    }
+    
+    public function desconectar(){
+        $this->conexao = NULL;
+    }
+
+    private function getHost() {
+        return $this->host;
+    }
+
+    private function getDbname() {
+        return $this->dbname;
+    }
+
+    private function getUsuario() {
+        return $this->usuario;
+    }
+
+    private function getSenha() {
+        return $this->senha;
+    }
+
+    function getTipo() {
+        return $this->tipo;
+    }
+
+    
+
+}
